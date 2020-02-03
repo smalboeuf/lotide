f1e5bcbf11cdd74fbbb64d70889da6841dbf6391
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-02-03 19:20:34 +0000

    made a head function that returns the first element of the passed array

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+function head(givenArray){
+
+  return givenArray[0];
+}
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
