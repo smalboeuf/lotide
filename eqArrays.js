10fe193c6c7d51b174888f604abab379f9981520
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-02-04 17:58:48 +0000

    Made a function that returns a boolean based on if the 2 given arrays are equal

---
+++ eqArrays.js
@@ -0,0 +1,42 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+function eqArrays(arrayOne, arrayTwo) {
+  let arrayOneNumbs = [];
+  let arrayTwoNumbs = [];
+
+  //Convert the arrays to numbers
+  for (let i = 0; i < arrayOne.length; i++) {
+    arrayOneNumbs[i] = Number(arrayOne[i]);
+  }
+
+  for (let i = 0; i < arrayTwo.length; i++) {
+    arrayTwoNumbs[i] = Number(arrayTwo[i]);
+  }
+
+  //making sure both arrays have the same length
+  if (arrayOneNumbs.length === arrayTwoNumbs.length) {
+
+    for (let i = 0; i < arrayOneNumbs.length; i++) {
+      if (arrayOneNumbs[i] !== arrayTwoNumbs[i]) {
+        return false;
+      }
+    }
+
+  } else {
+    return false;
+  }
+
+  return true;
+}
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
