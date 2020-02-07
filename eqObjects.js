7eca828834e81838733a8e785b65de7df3ea084a
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-02-07 20:18:25 +0000

    Used recursion to improve function

--- eqObjects.js
+++ eqObjects.js
@@ -57,9 +57,16 @@ const eqObjects = function(object1, object2) {
         return false;
       }
 
+    } else if (typeof(object1[key]) === "object") {
+      if (typeof(object2[key]) === "object") {
+        if (!eqObjects(object1[key], object2[key])) {
+          return false;
+        }
+      } else {
+        return false;
+      }
     } else {
       if (!(object2[key] && (object2[key] === object1[key]))) {
-        console.log("gothere");
         return false;
       }
     }
@@ -71,20 +78,25 @@ const eqObjects = function(object1, object2) {
 };
 
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); //True
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); //False
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); //False
+
+
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// console.log(eqObjects(ab, ba)); // => true
 
 
 
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+// const abc = { a: "1", b: "2", c: "3" };
+// console.log(eqObjects(ab, abc)); // => false
 
 
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false); // => false
